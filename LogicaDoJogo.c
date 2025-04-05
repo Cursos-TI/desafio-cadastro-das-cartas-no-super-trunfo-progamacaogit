03132473ee1bc80a7727fc42884d0348f767f3c3
progamacaogit <martinsgustavo181120gu@gmail.com>
2025-04-05 12:02:02 +0000

    jogo_atualizado

--- LogicaDoJogo.c
+++ LogicaDoJogo.c
@@ -1,4 +1,4 @@
-#include <stdio.#include <stdio.h>
+#include <stdio.h>
 
 int main() {
     // Declaração das variáveis
@@ -11,7 +11,7 @@ int main() {
     float densipopulacional1, densipopulacional2;
     float pibpercapita1, pibpercapita2;
     int SuperPoderCart1, SuperPoderCart2;
-
+    
     printf("\nJOGO SUPER TRUNFO!\n");
 
     // Entrada de dados para a primeira carta
@@ -24,7 +24,7 @@ int main() {
     scanf(" %s", codigo1);
 
     printf("Digite o nome da cidade: ");
-    scanf(" %s", nome_cidade1);
+    scanf(" %c", nome_cidade1);
 
     printf("Digite a população: ");
     scanf(" %lu", &populacao1);
@@ -55,7 +55,7 @@ int main() {
     scanf(" %s", codigo2);
 
     printf("Digite o nome da cidade: ");
-    scanf(" %s", nome_cidade2);
+    scanf(" %c", nome_cidade2);
 
     printf("Digite a população: ");
     scanf(" %lu", &populacao2);
@@ -79,53 +79,18 @@ int main() {
     // Cálculo do "Super Poder" das cartas
     SuperPoderCart1 = populacao1 + pontosTuristicos1 + (int)area1 + (int)pib1 + (int)pibpercapita1 + (int)densipopulacional1;
     SuperPoderCart2 = populacao2 + pontosTuristicos2 + (int)area2 + (int)pib2 + (int)pibpercapita2 + (int)densipopulacional2;
-
+//resultado dos poderes das cartas 1 e 2
     printf("\nSUPER PODER DAS CARTAS:\n");
     printf("Carta 1: %d\n", SuperPoderCart1);
     printf("Carta 2: %d\n", SuperPoderCart2);
 
-    // Comparações simples
-    printf("\nCOMPARAÇÃO DOS ATRIBUTOS (0 = perdeu, 1 = venceu):\n");
+    if(SuperPoderCart1 > SuperPoderCart2)
+        printf("A carta 1 venceu!\n");
+    else if(SuperPoderCart1 < SuperPoderCart2)
+        printf("A carta 2 venceu!\n");
+    else
+        printf("As cartas são iguais!\n");
 
-    printf("População: Carta 1 = %d | Carta 2 = %d\n",
-           populacao1 > populacao2 ? 1 : 0,
-           populacao2 > populacao1 ? 1 : 0);
-
-    printf("Pontos Turísticos: Carta 1 = %d | Carta 2 = %d\n",
-           pontosTuristicos1 > pontosTuristicos2 ? 1 : 0,
-           pontosTuristicos2 > pontosTuristicos1 ? 1 : 0);
-
-    printf("Área: Carta 1 = %d | Carta 2 = %d\n",
-           area1 > area2 ? 1 : 0,
-           area2 > area1 ? 1 : 0);
-
-    printf("PIB: Carta 1 = %d | Carta 2 = %d\n",
-           pib1 > pib2 ? 1 : 0,
-           pib2 > pib1 ? 1 : 0);
-
-    // Regra especial: MENOR densidade vence
-    printf("Densidade Populacional (menor vence): Carta 1 = %d | Carta 2 = %d\n",
-           densipopulacional1 < densipopulacional2 ? 1 : 0,
-           densipopulacional2 < densipopulacional1 ? 1 : 0);
-
-    printf("PIB per capita: Carta 1 = %d | Carta 2 = %d\n",
-           pibpercapita1 > pibpercapita2 ? 1 : 0,
-           pibpercapita2 > pibpercapita1 ? 1 : 0);
-
-    printf("Super Poder: Carta 1 = %d | Carta 2 = %d\n",
-           SuperPoderCart1 > SuperPoderCart2 ? 1 : 0,
-           SuperPoderCart2 > SuperPoderCart1 ? 1 : 0);
-
-    return 0;
-}
-
+ return 0;
 
 }
-
-
-
-
-
-
-
-    
